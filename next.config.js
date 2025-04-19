40017684b5bc12e4a6b82076a87e19422167a119
Linda Ayoola <lindaayoola@macbookair.home>
2025-04-19 17:25:06 +0100

    fix: Simplify Railway and Next.js configurations

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: true,
-  poweredByHeader: false,
   reactStrictMode: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
   experimental: {
     serverActions: true,
   },
